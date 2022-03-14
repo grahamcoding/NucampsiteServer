4344b09892ff8e9f254301154a5dfe5c074fdef4
Daniel Graham <dgraham.sprint@gmail.com>
2022-03-14 17:15:44 -0500

    File Uploader (Multer)

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const config = require('./config');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+const uploadRouter = require('./routes/uploadRouter');
 const campsiteRouter = require('./routes/campsiteRouter');
 const promotionRouter = require('./routes/promotionRouter');
 const partnerRouter = require('./routes/partnerRouter');
@@ -57,6 +57,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/campsites', campsiteRouter);
 app.use('/promotions', promotionRouter);
 app.use('/partners', partnerRouter);
+app.use('/imageUpload', uploadRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
